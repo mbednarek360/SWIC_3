de18074a9db4c20d0fe10057c9099614a1b6ef6d
Michael Bednarek <mbednarek360@protonmail.com>
2020-05-15 11:37:03 -0400

    Build Modifications

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ wasm-pack build --target web --release
 mv pkg web
 find src/pkg -type f ! -name '*.js' ! -name '*.wasm' -delete
 find src/pkg -type f ! -name '*.wasm' -exec minify {} -o {} \;
-find src -name "*.html" -o -name "*.js" -o -name "*.wasm" -o -name "*.css" | tar -cjf SLIC_3.tar.bz2 -T -
+find src -name "*.html" -o -name "*.js" -o -name "*.wasm" -o -name "*.css" | tar -cjf SWIC_3.tar.bz2 -T -
