5a2889579a2796c1208eaaf25130577f75947e41
Michael Bednarek <mbednarek360@protonmail.com>
2020-05-15 11:56:47 -0400

    Fixed package

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 rm -r web/pkg
 wasm-pack build --target web --release
 mv pkg web
-find src/pkg -type f ! -name '*.js' ! -name '*.wasm' -delete
-find src/pkg -type f ! -name '*.wasm' -exec minify {} -o {} \;
-find src -name "*.html" -o -name "*.js" -o -name "*.wasm" -o -name "*.css" | tar -cjf SWIC_3.tar.bz2 -T -
+find web/pkg -type f ! -name '*.js' ! -name '*.wasm' -delete
+find web/pkg -type f ! -name '*.wasm' -exec minify {} -o {} \;
+find web -name "*.html" -o -name "*.js" -o -name "*.wasm" -o -name "*.css" | tar -cjf SWIC_3.tar.bz2 -T -
